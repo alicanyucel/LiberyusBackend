c80e812bb5eb6e1d9f160262b7ace0ec455e3732
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-03 13:42:37 +0300

    ok

--- Liberyus.Application/Mapping/MapProfile.cs
+++ Liberyus.Application/Mapping/MapProfile.cs
@@ -9,7 +9,7 @@ using Liberyus.Domain.Entities;
 namespace Liberyus.Application.Mapping
 {
     internal sealed class MappingProfile : Profile
-    {
+    { //ok
         public MappingProfile()
         {
             CreateMap<CreateBlogCommand, Blog>().ReverseMap(); // best practice two way mapping
