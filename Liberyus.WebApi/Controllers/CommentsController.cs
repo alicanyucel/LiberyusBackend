9bd8756347770394fbe6f6703660d341d6ba2627
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-01-03 12:10:18 +0300

    copy paste

--- Liberyus.WebApi/Controllers/CommentsController.cs
+++ Liberyus.WebApi/Controllers/CommentsController.cs
@@ -1,5 +1,11 @@
-﻿using Liberyus.WebApi.Abstractions;
+﻿using Liberyus.Application.Features.Blogs.AddBlog;
+using Liberyus.Application.Features.Blogs.DeleteBlog;
+using Liberyus.Application.Features.Blogs.GetAllBlog;
+using Liberyus.Application.Features.Blogs.GetByIdBlog;
+using Liberyus.Application.Features.Blogs.UpdateBlog;
+using Liberyus.WebApi.Abstractions;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Liberyus.WebApi.Controllers;
 
@@ -8,5 +14,47 @@ public class CommentsController : ApiController
 {
     public CommentsController(IMediator mediator) : base(mediator)
     {
+    }
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        var request = new GetByIdCommentQuery(id);
+        var result = await _mediator.Send(request);
+
+        if (result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(result);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateBlogCommand request, CancellationToken cancellationToken)
+    {
+
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll([FromQuery] GetAllBlogQuery request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return Ok(response);
+
+    }
+    [HttpDelete]
+    public async Task<IActionResult> DeleteBlogById(DeleteBlogByIdCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateBlogById(UpdateBlogByIdCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+        return StatusCode(response.StatusCode, response);
+
     }
 }
